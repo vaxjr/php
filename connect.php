3006cba071121da494879ba06d206bdc3f238753
Victor <victorallen24@outlook.com>
2017-01-15 21:14:36 -0600

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,21 +1,18 @@
 <?php
 /**
  * Connect to the local database.
- * For practical applications, replace the sample text with real world values.
- * For example: 
+ * For practical applications, replace the sample text with real 
+ * world values. For example: 
  *  $host can be a hostname 'my_host' or IP address '127.0.0.1'
- *    For remote connections you need to add a port number such as 3306.
+ *    For remote connections add a port number such as 3306.
  *  $dbName is the name of the database such as 'my_db'
  *  $dbUser is your database user name such as 'lois_lane'
  *  $dbPwd is your database password such as 'dailyPlan3t!'
- *    For a remote database connection do not include the password.
- *    To avoid security issues, add some code to prompt for the password. 
- * 
+ *    For a secure remote connection, prompt the user for a password.
  */
- 
 $host = 'localhost';
-$dbUser = 'rootusr';
-$dbPwd = 'rootpwd';
+$dbUser = 'root';
+$dbPwd = 'root';
 $dbName = 'test';
  
 /** 
@@ -24,9 +21,9 @@ $dbName = 'test';
 $connection = mysqli_connect($host, $dbUser, $dbPwd, $dbName)or die(mysql_error());
 
 /**
-*  Make sure the connection succeeded. If the connection failed, display an error message.
+* Make sure the connection succeeded. If the connection failed, 
+* display an error message.
 */
-
 if (!$connect) {
     echo 'Error. Unable to connect to ' . $dbName . PHP_EOL;
     echo 'Debugging error number: ' . mysqli_connect_errno() . PHP_EOL;
@@ -35,18 +32,13 @@ if (!$connect) {
 }
 
 /**
-*  If the connection is made, display a success message.
+* If the connection is made, display a success message.
 */
-
 echo 'Success. Connected to:' . $dbName . PHP_EOL;
-echo "Host info: " . mysqli_get_host_info($connect) . PHP_EOL;
+echo 'Host info: ' . mysqli_get_host_info($connect) . PHP_EOL;
 
 /**
-* If you are done using the connection, close it using the mysqli_close function:
-* 
-*    mysqli_close($connect);
-* 
-* For this example we are leaving the connection open to perform some operations.
+* If you are done using the connection, close it using the mysqli_close function.
 */
-
+mysqli_close($connect);
 ?>
